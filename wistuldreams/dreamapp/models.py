4f6597aaee29d4fe98748909a8083808fa739b78
Snourry <30350968+Existrace@users.noreply.github.com>
2023-08-03 17:22:13 +0200

    Create models (Dream and Tag)

--- wistuldreams/dreamapp/models.py
+++ wistuldreams/dreamapp/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+CATEGORY_CHOICES = (
+    ("normal", "Normal"),
+    ("lucid", "Lucide"),
+    ("nightmare", "Cauchemar"),
+)
+
+MOOD_CHOICES = (
+    ("horrible", "Horrible"),
+    ("unpleasant", "Désagréable"),
+    ("nothing_special", "Rien de spécial"),
+    ("rather_nice", "Plutôt sympatique"),
+    ("nice", "Sympa"),
+    ("incredible", "Incroyable"),
+)
+
+
+class Dream(models.Model):
+    title = models.CharField(max_length=30)
+    content = models.TextField(max_length=300)
+    commentary = models.TextField(max_length=150)
+    is_recurring = models.BooleanField(default=False)
+    date_added = models.DateTimeField(auto_now_add=True)
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES)
+    mood = models.CharField(max_length=20, choices=MOOD_CHOICES)
+    user = models.ForeignKey(User, related_name="messages", on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.title
