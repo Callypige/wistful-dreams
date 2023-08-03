1da95b4bed45917061c07e7537a376f527a74457
Snourry <30350968+Existrace@users.noreply.github.com>
2023-08-03 16:55:42 +0200

    Login + sign up and create superuser

--- wistuldreams/dreamapp/views.py
+++ wistuldreams/dreamapp/views.py
@@ -1,5 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from .forms import SignUpForm
 
 
 def frontpage(request):
     return render(request, "dreamapp/frontpage.html")
+
+
+def signup(request):
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+
+        if form.is_valid():
+            user = form.save()
+
+            login(request, user)
+
+            return redirect("frontpage")
+    else:
+        form = SignUpForm()
+
+    return render(request, "dreamapp/signup.html", {"form": form})
